1a571fa4d71405f56fa4330e93f443ccf24c94ca
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2023-02-10 11:20:57 +0300

    minor test file edits

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,5 +1,5 @@
 import App from "../App";
-import { it, describe, test } from "vitest";
+import { it, describe, expect } from "vitest";
 import { render, screen, act } from "@testing-library/react";
 import { MemoryRouter as Router } from "react-router-dom";
 import { AuthContextProvider } from "../../context/AuthContext";
