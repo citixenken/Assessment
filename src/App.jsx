b4615fc0ce2405f6555608664c34c59d52ca77ae
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2023-02-13 07:36:55 +0300

    fix: code cleanup - handle API calls

--- src/App.jsx
+++ src/App.jsx
@@ -15,73 +15,73 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import ForgotPassword from "../screens/ForgotPassword";
 
 function App() {
-  const USERS_ENDPOINT = "https://jsonplaceholder.typicode.com/users";
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-    fetchUsers();
-  }, []);
+  // const USERS_ENDPOINT = "https://jsonplaceholder.typicode.com/users";
+  // const [users, setUsers] = useState([]);
+  // useEffect(() => {
+  //   fetchUsers();
+  // }, []);
 
-  // READ
+  // // READ
 
-  const fetchUsers = async () => {
-    try {
-      const response = await fetch(USERS_ENDPOINT);
-      const data = await response.json();
-      setUsers(data);
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
+  // const fetchUsers = async () => {
+  //   try {
+  //     const response = await fetch(USERS_ENDPOINT);
+  //     const data = await response.json();
+  //     setUsers(data);
+  //   } catch (error) {
+  //     console.error("Error:", error);
+  //   }
+  // };
 
-  // CREATE => POST
-  const addUser = async (newUser) => {
-    await fetch(USERS_ENDPOINT, {
-      method: "POST",
-      body: JSON.stringify(newUser),
-      headers: { "Content-Type": "application/json; charset=UTF-8" },
-    })
-      .then((res) => {
-        //res 201 - Created OK
-        if (res.status !== 201) {
-          return;
-        } else {
-          return res.json();
-        }
-      })
-      .then((data) => {
-        setUsers((users) => [data, ...users]);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+  // // CREATE => POST
+  // const addUser = async (newUser) => {
+  //   await fetch(USERS_ENDPOINT, {
+  //     method: "POST",
+  //     body: JSON.stringify(newUser),
+  //     headers: { "Content-Type": "application/json; charset=UTF-8" },
+  //   })
+  //     .then((res) => {
+  //       //res 201 - Created OK
+  //       if (res.status !== 201) {
+  //         return;
+  //       } else {
+  //         return res.json();
+  //       }
+  //     })
+  //     .then((data) => {
+  //       setUsers((users) => [data, ...users]);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // };
 
-  // DELETE
-  const deleteUser = async (id) => {
-    await fetch(`${USERS_ENDPOINT}/${id}`, { method: "DELETE" })
-      .then((res) => {
-        if (res.status !== 200) {
-          return;
-        } else {
-          setUsers(
-            users.filter((user) => {
-              return user.id !== id;
-            })
-          );
-        }
-      })
-      .catch((err) => console.log(err));
-  };
+  // // DELETE
+  // const deleteUser = async (id) => {
+  //   await fetch(`${USERS_ENDPOINT}/${id}`, { method: "DELETE" })
+  //     .then((res) => {
+  //       if (res.status !== 200) {
+  //         return;
+  //       } else {
+  //         setUsers(
+  //           users.filter((user) => {
+  //             return user.id !== id;
+  //           })
+  //         );
+  //       }
+  //     })
+  //     .catch((err) => console.log(err));
+  // };
 
-  const user = users.map((user) => (
-    <User
-      id={user.id}
-      key={user.id}
-      name={user.name}
-      email={user.email}
-      onDelete={deleteUser}
-    />
-  ));
+  // const user = users.map((user) => (
+  //   <User
+  //     id={user.id}
+  //     key={user.id}
+  //     name={user.name}
+  //     email={user.email}
+  //     onDelete={deleteUser}
+  //   />
+  // ));
 
   return (
     <div className="App">
